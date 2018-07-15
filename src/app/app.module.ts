edf4277adc279ceea9d9fb94e67b621998ff2f50
Aleksei Maratkanov <aleksei.maratkanov@iii.com>
2018-07-15 18:40:22 +0300

    Fix imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { MatCardModule } from '@angular/material/card';
 
 import { AppComponent } from './app.component';
 import { ProductsService } from './products/services/products.service';
 
 import { CartModule } from './cart/cart.module';
+import { ProductsModule } from './products/products.module';
 
 @NgModule({
   declarations: [
@@ -13,8 +13,8 @@ import { CartModule } from './cart/cart.module';
   ],
   imports: [
     BrowserModule,
-    MatCardModule,
-    CartModule
+    CartModule,
+    ProductsModule
   ],
   providers: [
     ProductsService,
--- src/app/products/products.module.ts
+++ src/app/products/products.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MatCardModule } from '@angular/material/card';
 
 import { ProductComponent } from './components/product/product.component';
 import { ProductListComponent } from './components/product-list/product-list.component';
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    MatCardModule
   ],
   declarations: [ProductComponent, ProductListComponent],
   exports: [ProductComponent, ProductListComponent],
