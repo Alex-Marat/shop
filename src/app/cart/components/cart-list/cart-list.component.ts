d2ba9eaa885c2371d186681039d38d7b1afaff2b
Aleksei Maratkanov <aleksei.maratkanov@iii.com>
2018-07-21 14:15:13 +0300

    Migrate to Angular 6

--- src/app/cart/components/cart-list/cart-list.component.ts
+++ src/app/cart/components/cart-list/cart-list.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 
-import { Subscription } from 'rxjs/Subscription';
+import { Subscription } from 'rxjs';
 
 import { ProductModel } from '../../../products/product.model';
 import { CartService } from '../../services/cart.service';
